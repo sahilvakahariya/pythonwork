4c9920ee06291f6e5dc1251326733f3658dcadde
SAHIL PATEL <sahilvakahariya@gmail.com>
2026-04-23 11:57:23 +0530

    second

---
+++ CLASSWORK/005.collection/setdemo.py
@@ -0,0 +1,26 @@
+a={10,20,30,40,50,True,1}
+b={30,40,50,60,70,0,False}
+
+# print(a.update(b)) 
+# print(a)
+
+# k=a.union(b)
+# k=a|b
+# print(a)
+# print(k)
+
+# # k=a.intersection(b)        
+# k=a.intersection_update(b)
+# k=a&b
+# print(a)
+
+# k=a.difference(b)
+# # k=a.difference_update(b)
+# k=a-b
+# print(a)
+
+# k=a.symmetric_difference(b)
+# k=a.symmetric_difference_update(b)
+# k=a^b
+# print(a)
+
