6e0879f3212649e4bf5f1c3271c0736673786a53
SAHIL PATEL <sahilvakahariya@gmail.com>
2026-04-23 19:38:44 +0530

    all

--- CLASSWORK/005.collection/setdemo.py
+++ CLASSWORK/005.collection/setdemo.py
@@ -1,3 +1,16 @@
+s  = {10,20,30,40,50,50}
+
+for i in s:
+    print(i)
+
+s.add(100)
+print(s)
+
+s.remove(100)
+s.discard(50)
+s.pop()
+print(s)
+
 a={10,20,30,40,50,True,1}
 b={30,40,50,60,70,0,False}
 
@@ -9,13 +22,13 @@ k=a|b
 print(a)
 print(k)
 
-# k=a.intersection(b)        
+k=a.intersection(b)        
 k=a.intersection_update(b)
 k=a&b
 print(a)
 
 k=a.difference(b)
-# k=a.difference_update(b)
+k=a.difference_update(b)
 k=a-b
 print(a)
 
---
+++ CLASSWORK/005.collection/tupledemo.py
@@ -0,0 +1,29 @@
+t = (10,20,30,40,50,"ddsd",56.44)
+t = tuple((10,20,30))
+print(t)
+print(type(t))
+print(len(t))
+
+
+t = ("python","java","php","android")
+
+k = list(t)
+k.append("React")
+t = tuple(k)
+print(t)
+
+
+(*a,b) = ("python","java","php","android")
+
+print(a)
+print(b)
+
+print(t*2)
+
+# del t
+# print(t)
+
+k = [10]
+print(type(k))
+l = (10,)
+print(type(l))
